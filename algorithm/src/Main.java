15ab40e1e9b7e9f3ea7d180d3dba767244cd93a7
brighter08iz <brighter08iz@gmail.com>
2023-11-25 23:19:42 +0900

    [문자열] 1. 문자 찾기

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+    public int solution(String str, char c) {
+        int answer = 0;
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+
+        for(char x : str.toCharArray()) {
+            if(x==c) {
+                answer ++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        char c = scanner.next().charAt(0);
+
+        System.out.print(T.solution(str, c));
+    }
 }
